f8cfc9472cedc484494c31c9ae4aa3bc509b978c
copaiement <cole.paiement@gmail.com>
2024-01-24 14:38:53 -0800

    Some small style updates

--- models/message.js
+++ models/message.js
@@ -7,16 +7,9 @@ const MessageSchema = new Schema({
   text: { type: String, required: true },
   username: { type: String, required: true },
   addedDate: { type: Date, required: true },
-  userType: { type: String, required: true },
 });
 
-// Virtual for book's URL
-MessageSchema.virtual('url').get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/book/${this._id}`;
-});
-
-MessageSchema.virtual('post_date_formatted').get(function () {
+MessageSchema.virtual('addedDate_formatted').get(function () {
   return DateTime.fromJSDate(this.added).toLocaleString(DateTime.DATETIME_MED);
 });
 
--- models/user.js
+++ models/user.js
@@ -3,18 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-});
-
-// Virtual for book's URL
-MessageSchema.virtual('url').get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/book/${this._id}`;
-});
-
-MessageSchema.virtual('post_date_formatted').get(function () {
-  return DateTime.fromJSDate(this.added).toLocaleString(DateTime.DATETIME_MED);
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  userType: { type: String, required: true, enum: ['basic', 'member', 'admin'] },
 });
 
 // Export model
